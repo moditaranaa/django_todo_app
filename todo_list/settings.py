f4eae1f1a14f2ff06661451e5f5f0ffb617b371b
em <starkastic@Stark-PC>
2022-12-29 21:21:44 +0530

    added depl changes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -119,5 +119,9 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     STATIC_ROOT,
+#     STATIC_URL
+# ]
